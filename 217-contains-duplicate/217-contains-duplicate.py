e1316fb6cad6360d5065b43002eaa9a94b4927c0
Josiah Leon <sjosiahleon@gmail.com>
2022-08-25 08:38:14 -0700

    Time: 815 ms (25.95%), Space: 25.9 MB (71.55%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.py
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -1,9 +1,22 @@
 from collections import defaultdict as ddict
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        n = ddict(int)
+        n = dict()
+        
         for num in nums:
-            n[num] += 1
+            if num not in n:
+                n[num] = 1
+            else:
+                n[num] += 1
+                
             if n[num] >= 2:
                 return True
         return False
+        
+        
+        # n = ddict(int)
+        # for num in nums:
+        #     n[num] += 1
+        #     if n[num] >= 2:
+        #         return True
+        # return False
