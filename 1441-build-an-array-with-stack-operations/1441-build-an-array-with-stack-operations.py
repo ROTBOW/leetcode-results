da099eb10b2866bd6cee0e6f097ee68b99538ef0
Josiah Leon <sjosiahleon@gmail.com>
2022-09-07 09:20:47 -0700

    Time: 63 ms (22.67%), Space: 14 MB (18.38%) - LeetHub

--- 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.py
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.py
@@ -1,9 +1,9 @@
 class Solution:
     def buildArray(self, target: List[int], n: int) -> List[str]:
-        res = [] # [1, 3] - [P, P, p]
-        arr = [] # [1]
-        idx = 0 # 1
-        num = 1 # 3
+        res = []
+        arr = []
+        idx = 0
+        num = 1
         while idx <= len(target)-1 and num <= n:
             arr.append(num)
             res.append('Push')
