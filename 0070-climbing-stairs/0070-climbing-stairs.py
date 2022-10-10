0559eccaf8db5b8486c74b7cca1d41d409198ae8
Josiah Leon <sjosiahleon@gmail.com>
2022-10-10 07:59:31 -0700

    Time: 63 ms (15.28%), Space: 13.9 MB (11.70%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,20 @@
+from functools import lru_cache
+
+class Solution:
+    
+    @lru_cache
+    def climbStairs(self, n: int) -> int:
+        if n <= 2:
+            return n if n >= 0 else 0
+        
+        n1, n2 = 2, 1
+        ans = 0
+        
+        for step in range(2, n):
+            ans = n1 + n2
+            n2 = n1
+            n1 = ans
+            
+        return ans
+        
+        
