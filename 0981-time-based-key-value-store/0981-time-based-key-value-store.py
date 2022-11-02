29ebf19366024c49affde4fc41da91289f171e52
Josiah Leon <sjosiahleon@gmail.com>
2022-11-02 07:44:19 -0700

    Time: 1571 ms (57.28%), Space: 72.3 MB (22.85%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.py
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.py
@@ -4,17 +4,14 @@ import heapq
 class TimeMap:
 
     def __init__(self):
-        self.data = ddict(dict)
+        self.data = ddict(list)
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-        if key not in self.data:
-            self.data[key] = []
         heapq.heappush(self.data[key], [timestamp, value])
 
     def get(self, key: str, timestamp: int) -> str:
         data = self.data[key]
         for idx in range(len(data)-1, -1, -1):
-            # item = data.pop()
             item = data[idx]
             if item[0] <= timestamp:
                 return item[1]
