50cea898e11179ccb6b843f5b64ac1ae4af9ad5e
Josiah Leon <sjosiahleon@gmail.com>
2023-08-08 07:23:31 -0700

    Time: 1071 ms (21.83%) | Memory: 16.4 MB (85.37%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,14 +1,15 @@
+from collections import Counter
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        s1 = sorted(s1)
         l, r = 0, len(s1)
+        s1 = Counter(s1)
         
         while r <= len(s2):
-            if s1 == sorted(s2[l:r]):
+            if s1 == Counter(s2[l:r]):
                 return True
             l += 1
             r += 1
-        
+            
         return False
 
 
