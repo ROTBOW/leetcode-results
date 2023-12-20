fd8481386e7b7ff3c916f6047641abf4e94509e9
Josiah Leon <sjosiahleon@gmail.com>
2023-12-20 15:47:31 -0800

    Time: 58 ms (89.08%) | Memory: 17.2 MB (25.45%) - LeetSync

---
+++ 2756-buy-two-chocolates/buy-two-chocolates.py
@@ -0,0 +1,16 @@
+class Solution:
+    def buyChoco(self, prices: List[int], money: int) -> int:
+        min_two = [float('inf'), float('inf')]
+
+        for choco in prices:
+            if choco <= min_two[0]:
+                min_two[1] = min(min_two[1], min_two[0])
+                min_two[0] = choco
+            elif choco <= min_two[1]:
+                min_two[1] = choco
+        print(min_two)
+        if sum(min_two) > money:
+            return money
+        return abs(sum(min_two) - money)
+                
+        
