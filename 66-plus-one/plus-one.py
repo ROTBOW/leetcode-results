e27ceb4329fdfb8f8a6ed0e8b0556d02e31f4008
Josiah Leon <sjosiahleon@gmail.com>
2024-07-26 14:40:45 -0700

    Time: 39 ms (39.92%) | Memory: 16.5 MB (58.99%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -10,5 +10,20 @@ class Solution:
 
         if digits[0] == 0:
             return [1] + digits
-            
+
         return digits
+
+        # two cases
+        # looking for theses cases while loop backwards
+        # digits[i] == 9 OR not be 9
+
+        # case 1, it is 9
+        # change 9 to 0, and let our loop continue
+
+        # case 2, it is NOT 9
+        # up the number by one at that index, and return digits
+
+        # if digits is only 9s, ie like [9, 9]
+        # so after its gone through it would look like this [0, 0]
+        # so if our first number, after looping is zero, we know that we need to add a 1
+        # we can return [1] + digits ([0, 0]) => [1, 0, 0]
