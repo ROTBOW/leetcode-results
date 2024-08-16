c9a0b60c28bb568f51cd9f1bb8964111c94a6a27
Josiah Leon <sjosiahleon@gmail.com>
2024-08-16 14:01:39 -0700

    Time: 129 ms (28.22%) | Memory: 20.5 MB (78.29%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -1,16 +1,24 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        intervals.sort(reverse=True) # O(n log n)
-        answer = []
-        
+        # we to sort our intervals
+        # when we sort, reverse it
+        # and I do this, so I treat it like a stack
+        # create a var res, this will be what we return
+
+        # while our stack (intervals) has at least 1 interval - which means our will end when there is only one left
+        #   if the curr top of stack can merged with what is directly below it, we merge them to the one below it, and pop off the top
+        #   else pop off the top and append to res
+
+        # when our loop ends we're return rez + intervals
+        intervals.sort(reverse=True)
+        res = list()
+
         while len(intervals) > 1:
-            if intervals[-1][1] >= intervals[-2][0]:
-                intervals[-2][0] = min(intervals[-1][0], intervals[-2][0])
-                intervals[-2][1] = max(intervals[-1][1], intervals[-2][1])
-                intervals.pop()
-            else:
-                answer.append(intervals.pop())
-                
-        return answer + intervals
-                
-        
+            top = intervals.pop()
+            if top[1] >= intervals[-1][0]: # this is the care we merge
+                intervals[-1][0] = min(intervals[-1][0], top[0])
+                intervals[-1][1] = max(intervals[-1][1], top[1])
+            else: # so we can't merge
+                res.append(top)
+
+        return res + intervals
